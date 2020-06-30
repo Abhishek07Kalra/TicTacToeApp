78091a88ff832fa04bd7c219a67e09c1e41559a4
Abhishek kalra <iamabhishekkalra@gmail.com>
2020-06-30 16:03:29 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/example/tictactoeapp/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.tictactoeapp
-
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.collections.ArrayList
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +19,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         button10.setOnClickListener {
             reset()
 
@@ -44,29 +48,45 @@ class MainActivity : AppCompatActivity() {
     var emptyCells = ArrayList<Int>()
     var activeUser = 1
     fun playnow(buttonSelected:Button , currCell:Int)
-    {
+    {   val audio = MediaPlayer.create(this , R.raw.poutch)
         if(activeUser == 1)
         {
             buttonSelected.text = "X"
-            activeUser = 2
+            buttonSelected.setTextColor(Color.parseColor("#EC0C0C"))
             player1.add(currCell)
             emptyCells.add(currCell)
+            audio.start()
+            //Handler().postDelayed(Runnable { audio.pause() } , 500)
             buttonSelected.isEnabled = false
-            checkwinner()
+            Handler().postDelayed(Runnable { audio.release() } , 200)
+            val checkWinner = checkwinner()
+            if(checkWinner == 1){
+                reset()
+            }
+            else if(singleUser)
+                robot()
+            else
+                activeUser = 2
 
         }
         else
         {
             buttonSelected.text = "O"
+            audio.start()
+            buttonSelected.setTextColor(Color.parseColor("#D22BB804"))
+            //Handler().postDelayed(Runnable { audio.pause() } , 500)
             activeUser = 1
             player2.add(currCell)
             emptyCells.add(currCell)
+            Handler().postDelayed(Runnable { audio.release() } , 200)
             buttonSelected.isEnabled = false
-            checkwinner()
+            var checkWinner  = checkwinner()
+            if(checkWinner == 1)
+                reset()
         }
 
     }
-    fun checkwinner()
+    fun checkwinner():Int
     {
 
         if((player1.contains(1) && player1.contains(2) && player1.contains(3) ) || (player1.contains(1) && player1.contains(4) && player1.contains(7))||
@@ -84,6 +104,9 @@ class MainActivity : AppCompatActivity() {
                 exitProcess(1)
             }
             build.show()
+            return 1
+
+
         }
             else if((player2.contains(1) && player2.contains(2) && player2.contains(3) ) || (player2.contains(1) && player2.contains(4) && player2.contains(7))||
                 (player2.contains(3) && player2.contains(6) && player2.contains(9)) || (player2.contains(7) && player2.contains(8) && player2.contains(9))||
@@ -100,6 +123,7 @@ class MainActivity : AppCompatActivity() {
                     exitProcess(1)
                 }
             build.show()
+            return 1
         }
         else if(emptyCells.contains(1) && emptyCells.contains(2) && emptyCells.contains(3) && emptyCells.contains(4) && emptyCells.contains(5) && emptyCells.contains(6) && emptyCells.contains(7) &&
             emptyCells.contains(8) && emptyCells.contains(9) ) {
@@ -114,7 +138,10 @@ class MainActivity : AppCompatActivity() {
                 exitProcess(1)
             }
             build.show()
+            reset()
+
         }
+        return 0
     }
 
     fun reset()
@@ -142,6 +169,37 @@ class MainActivity : AppCompatActivity() {
             buttonselected.text = ""
             textView.text = "Player1 : $player1Count"
             textView2.text = "Player2 : $player2Count"
+        }
+    }
+
+    fun robot()
+    {
+        val rnd = (1..9).random()
+        if(emptyCells.contains(rnd))
+            robot()
+        else {
+                var buttonselected : Button?
+                buttonselected = when(rnd) {
+                    1 -> button
+                    2 -> button2
+                    3 -> button3
+                    4 -> button4
+                    5 -> button5
+                    6 -> button6
+                    7 -> button7
+                    8 -> button8
+                    9 -> button9
+                    else -> {button}
+                }
+            emptyCells.add(rnd);
+            buttonselected.text = "O"
+            buttonselected.setTextColor(Color.parseColor("#D22BB804"))
+            player2.add(rnd)
+            buttonselected.isEnabled = false
+            var checkWinner = checkwinner()
+            if(checkWinner == 1)
+                reset()
+
         }
     }
 }
